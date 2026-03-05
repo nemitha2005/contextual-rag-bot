1a46aad52b1922999740e7d932169f3ba5bda7d7
nemitha2005 <nemithan05@gmail.com>
2026-03-05 20:37:29 +0530

    Fix New Chat button position back to left side

--- components/chat-header.tsx
+++ components/chat-header.tsx
@@ -43,7 +43,7 @@ function PureChatHeader({
 
       {(!open || windowWidth < 768) && (
         <Button
-          className="order-2 ml-auto h-8 px-2 md:order-1 md:ml-0 md:h-fit md:px-2"
+          className="h-8 px-2 md:h-fit md:px-2"
           onClick={() => {
             router.push("/");
             router.refresh();
